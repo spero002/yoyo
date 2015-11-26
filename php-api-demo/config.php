40568fc1743a864256e0bcd7e2168dcb277fb2e1
Xiong Gao <growdane@gmail.com>
2015-11-26 11:47:08 +0800

    delete config.php

--- php-api-demo/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* define public key and private key */
-define('PUBLIC_KEY', "ucloudhsium.gao@ucloud.cn14357349890002109138237"); 
-define('PRIVATE_KEY', "240a131b1eabded7099a75e15df9558bda104b47");
-
-/* base url for api request */
-define('BASE_URL', "https://api.ucloud.cn");
-?>
