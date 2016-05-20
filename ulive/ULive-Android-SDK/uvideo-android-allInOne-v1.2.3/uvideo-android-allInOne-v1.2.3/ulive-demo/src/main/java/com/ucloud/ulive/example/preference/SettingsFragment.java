d78b8c3b58260706ba1b0e797f1a7ecf6e9672df
gao <growdane@gmail.com>
2016-05-20 10:29:30 +0800

    add ulive sdk

---
+++ ulive/ULive-Android-SDK/uvideo-android-allInOne-v1.2.3/uvideo-android-allInOne-v1.2.3/ulive-demo/src/main/java/com/ucloud/ulive/example/preference/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.ucloud.ulive.example.preference;
+
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.ucloud.ulive.example.R;
+
+
+public class SettingsFragment extends PreferenceFragmentCompat{
+    public static final String TAG = "SettingsFragment";
+
+    public static SettingsFragment newInstance() {
+        SettingsFragment f = new SettingsFragment();
+        return f;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
---
+++ ulive/ULive-Android-SDK/uvideo-android-allInOne-v1.2.3/uvideo-android-allInOne-v1.2.3/uvod-demo/src/main/java/com/ucloud/uvod/example/AppConetxt.java
@@ -0,0 +1,10 @@
+package com.ucloud.uvod.example;
+
+import android.app.Application;
+
+public class AppConetxt extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
